a49a55812a23c8a5c6c4b0f3259ccbb2bac1b1ed
Aditya0423 <aditya.rastogi2410@gmail.com>
2022-09-07 18:11:21 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React, { Component }  from 'react';
+import React  from 'react';
 import { makeStyles } from "@material-ui/core";
 import Homepage from "./Pages/HomePage";
 
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,4 +1,4 @@
-import React, { Component }  from 'react';
+import React from 'react';
 import { Container, makeStyles, Typography } from "@material-ui/core";
 import Carousel from "./Carousel";
 
